61c8d6bf4c8aa281c61a3d0963d0e28629f7eb17
ragnarov <101@mailu.io>
2023-02-18 16:03:37 +0600

    Changes to be committed:       modified:   netwmicon.h

--- netwmicon.h
+++ netwmicon.h
@@ -2,5 +2,5 @@ extern const char  _binary_icon_png_size;
 extern const char  _binary_icon_png_start;
 extern const char  _binary_icon_png_end;
 
-const char *png_file_Ptr = &_binary_icon_png_start;
+char *png_file_Ptr = &_binary_icon_png_start;
 size_t png_file_size = (size_t) &_binary_icon_png_size;  /* Data size */
